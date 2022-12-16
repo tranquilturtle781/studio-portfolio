0d9e2cb110173c46d407f9f137c4ad6051bb18ae
tranquilturtle781 <115186235+tranquilturtle781@users.noreply.github.com>
2022-12-16 04:03:09 -0500

    Add files via upload

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -9,16 +9,23 @@ export default function Project1() {
                 <div className="toptext">
                 <p>How much can we learn by observing and talking to real users? This project explores just that by attempting to step into users' shoes and learn from their interactions with a specific interface.</p>
                 <p>My chosen interface was an elevator control panel located in the main elevator of a building at Brown University.</p>
+                </div>
                 <div className="f">
                 <img src="interface.png"></img>
                 </div>
              </div>
              <p>After both interviewing users as well as observing them in action, I generalized my findings in the form of two distinct personas, Friendy Fiona and Tired TA Tom. </p>
-             <img src="persona1.png"></img>
-             <p>Friendy Fiona represents the inexperienced users that don't use this elevator very frequently. She illustrates the struggles users shared regarding unintuitive buttons. </p>
-             <p>TA Tom is a busy student who is always running late. He represents the frequent users of this elevator that continue to struggle with the card swiping feature of the interface 
-                due to a lack of clear instructions.</p>
-            <img src="persona2.png"></img>
+             <div className="moreFlex">
+                <div className="moreFlexPart">
+                    <img src="persona1.png"></img>
+                    <p>Friendy Fiona represents the inexperienced users that don't use this elevator very frequently. She illustrates the struggles users shared regarding unintuitive buttons. </p>
+                </div>
+                <div className="moreFlexPart">
+                    <img src="persona2.png"></img>
+                    <p>TA Tom is a busy student who is always running late. He represents the frequent users of this elevator that continue to struggle with the card swiping feature of the interface 
+                    due to a lack of clear instructions.</p>
+                </div>
+             </div>
             <p>After developing these personas, I chose to really dig into the Tired TA Tom persona by creating a storyboard that documents his user journey. </p>
              <div class="display">
                 <div class="card1">
@@ -62,12 +69,12 @@ export default function Project1() {
                     <p>Tom finally makes it to his hours, flustered and late. Five students are already waiting for his help.</p>
                 </div>
             </div>
-             </div>
              <h2>Key Takeaways</h2>
              <p> In this project, I learned how to view an interface from the perspective of real users. I demonstrated my ability to learn
                 from users via a contextual inquiry, including writing questions for users that are stimulating yet objective. I also learned how to 
                 synthesize the information gained from users, and 
                 </p>
+                <br></br>
             <form action="https://tranquilturtle781.github.io/personas/" target="_blank">
                     <button type="submit" class="clicky">Explore the full project</button>
              </form>
